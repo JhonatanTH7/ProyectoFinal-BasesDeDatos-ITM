dfb15e5533510296bf57e257d0a622a615c20830
JhonatanTH7 <Jhonatantorohurtado93@gmail.com>
2024-11-17 12:48:34 -0500

    Archivo Consultas

---
+++ Consultas Proyecto Final Jhonatan.sql
@@ -0,0 +1,27 @@
+
+---------------------- CONSULTAS A LA BASE DE DATOS - PROYECTO FINAL JHONATAN TORO ----------------------
+
+/* D.	Hacer 4 consultas útiles para el usuario, que implementen los 4 tipos de Joins que hay. 
+Cada consulta debe ir con su respectivo enunciado, y debe generar algún resultado. */
+
+
+/* E.	Desarrollar 2 funciones de tablas y dos funciones escalares dentro de las necesidades del 
+proyecto a desarrollar. Estos procedimientos deben estar debidamente documentados. */
+
+
+/* F.	Hacer 2 consultas que utilice teoría de conjuntos. Deben ir con su enunciado y producir 
+algún resultado. */
+
+
+/* G.	Implementar dos vistas útiles para el negocio. Dichas vistas deben ser actualizables, y 
+deben tener 4 tablas base como mínimo. */
+
+
+/* H.	Generar los procedimientos almacenados necesarios para hacerle CRUD a MINIMO 2 TABLAS las 
+tablas del proyecto. Estos procedimientos deben estar debidamente documentados. */
+
+
+/* I.	Generar una necesidad dentro del negocio que implique hacer un procedimiento almacenado que 
+se integre con funciones de usuario y disparen Triggers (mínimo 2). No olviden usar control de 
+errores, usar cursores en lo posible y conservar la atomicidad. */
+
